c6f297f8aad98d2c6fbf9f00fbb5165f4acb657f
Kundan Kumar Gupta <kundankrgupta@outlook.com>
2024-07-31 16:51:53 +0530

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -116,9 +116,9 @@ const Navbar = () => {
 							target="_blank"
 							rel="noopener noreferrer"
 							download="kundan_kumar_gupta"
+							onClick={handleDownload}
 						>
-							<Button
-								onClick={handleDownload}
+							{/* <Button
 								px={{ base: "15px", sm: "15px", md: "30px", lg: "20px" }}
 								py={{ base: "15px", sm: "25px", md: "30px", lg: "20px" }}
 								borderRadius="5px"
@@ -134,8 +134,8 @@ const Navbar = () => {
 								textTransform={'uppercase'}
 								_hover={'none'}
 							>
-								resume
-							</Button>
+							</Button> */}
+							resume
 						</a>
 						<Spacer />
 						<IconButton fontSize='2xl' variant='ghost' onClick={onOpen} display={{ lg: "none" }}><FaBars />
